a0ee43d453e0d8befeecae849b1f90ae2c7fd004
Oluwatobiloba Olusoga <oluwatobilobaolusoga@gmail.com>
2019-02-04 12:21:24 +0100

    Update day15.js

--- day15.js
+++ day15.js
@@ -1,5 +1,8 @@
 function paginate(pageControl){
     let prevPageNo = parseInt(sessionStorage.getItem('pageNum'));
+    if (pageControl.id == prevPageNo){
+        return;
+    }
     if (pageControl.id == 'prev' && prevPageNo > 1){
         sessionStorage.setItem('pageNum', prevPageNo - 1);
     }
